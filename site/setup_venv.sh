869d798d0a52d646af27ed281165f43673042804
Sven Jacobs <github@svenjacobs.com>
2025-06-12 20:15:53 +0200

    chore: update workflows (#27)

--- site/setup_venv.sh
+++ site/setup_venv.sh
@@ -8,7 +8,7 @@ fi
 if [ ! -f .venv/bin/mkdocs ]; then
   echo "Installing mkdocs-material…"
   source .venv/bin/activate
-  pip install mkdocs-material[recommended] mkdocs-macros-plugin
+  pip install -r requirements.txt
 fi
 
 echo "Done!"
